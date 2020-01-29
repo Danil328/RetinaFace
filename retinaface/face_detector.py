1fc1e203b61d426cc2f816affef7d7c9b3dee5ee
danil328 <danil328@yandex.ru>
2020-01-29 15:49:27 +0300

    add scales

--- retinaface/face_detector.py
+++ retinaface/face_detector.py
@@ -9,7 +9,7 @@ from .retinaface import RetinaFace
 class FaceDetector(object):
     def __init__(self, weights: str = "https://drive.google.com/uc?id=11Ksdq9sQLp9E6WFuGzOZ-3jxTpx1nFCI",
                  threshold: float = 0.8,
-                 gpuid: int = -1, padding: float = 0.2, flip: bool = False, scales=None):
+                 gpuid: int = -1, padding: float = 0.2, flip: bool = False, scales=None, output="crop"):
 
         if scales is None:
             scales = [1024, 1980]
@@ -19,6 +19,7 @@ class FaceDetector(object):
         self.scales = scales
         self.target_size = scales[0]
         self.max_size = scales[1]
+        self.output = output
 
         if 'http' in weights:
             weights = self.download_weights(weights)
@@ -44,13 +45,19 @@ class FaceDetector(object):
         scales = [im_scale]
         faces, landmarks = self.detector.detect(image, self.threshold, scales=scales, do_flip=self.flip)
 
-        crops = []
+        outputs = []
         if faces is not None:
             for i in range(faces.shape[0]):
                 b = faces[i].astype(np.int)
                 padding_height = int((b[2] - b[0]) * self.padding)
                 padding_width = int((b[3] - b[1]) * self.padding)
+                b[1] = max(int(b[1] - padding_height), 0)
+                b[3] = int(b[3]) + padding_height
+                b[0] = max(int(b[0]) - padding_width, 0)
+                b[2] = int(b[2]) + padding_width
 
-                crops.append(image[max(int(b[1] - padding_height), 0):int(b[3]) + padding_height,
-                             max(int(b[0]) - padding_width, 0):int(b[2]) + padding_width, :])
-        return crops
+                if self.output == 'crop':
+                    outputs.append(image[b[1]:b[3], b[0]:b[2], :])
+                elif self.output == 'box':
+                    outputs.append(b)
+        return outputs
